b1649cd1ad2f9b0b38b37f6c4549ca8eacedb2aa
Maksim <tsarykmk@gmail.com>
2019-12-03 09:48:36 +0300

    add toolbar for app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
+        AppRoutingModule,
+        BrowserAnimationsModule,
         BrowserModule,
-        AppRoutingModule
+        MatToolbarModule
     ],
-    providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
