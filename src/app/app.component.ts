5ae32954930d7852c83248aae6853034bc1cdd1f
Maksim <tsarykmk@gmail.com>
2019-12-05 12:18:49 +0300

    Implement dashboard page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-
-import { ApiService } from './services/api.service';
-import { RepositoryModel } from './models/repository.model';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     githubLink = 'https://github.com/Godel';
-    repositoryList: RepositoryModel[];
-    filteredRepositoryList: RepositoryModel[];
-    searchControl = new FormControl();
-
-    constructor(private apiService: ApiService) {
-    }
-
-    ngOnInit(): void {
-        this.apiService.getRepositoryList().subscribe((x: RepositoryModel[]) => {
-            this.repositoryList = x;
-            this.filteredRepositoryList = this.repositoryList;
-        });
-        this.searchControl.valueChanges.subscribe((term: string) => this.onSearchTermChange(term));
-    }
-
-    onDetailsOpen(id: number): void {
-        console.log(id, 'coming soon!');
-    }
-
-    onSearchTermChange(term: string): void {
-        if (!term) {
-            this.filteredRepositoryList = this.repositoryList;
-        } else {
-            this.filteredRepositoryList = this.repositoryList.filter(x =>
-                x.name && x.name.toLowerCase().includes(term.toLowerCase())
-                || x.language && x.language.toLowerCase().includes(term.toLowerCase())
-            );
-        }
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import {
     MatButtonModule,
-    MatCardModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatListModule,
     MatToolbarModule
 } from '@angular/material';
 import { NgModule } from '@angular/core';
@@ -15,10 +15,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from './services/api.service';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        DashboardComponent
     ],
     imports: [
         AppRoutingModule,
@@ -26,10 +28,10 @@ import { AppRoutingModule } from './app-routing.module';
         BrowserModule,
         HttpClientModule,
         MatButtonModule,
-        MatCardModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
+        MatListModule,
         MatToolbarModule,
         ReactiveFormsModule
     ],
